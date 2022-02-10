2f21c91e90aff1b7994bbd24126c6bb0c9b047b6
Kate Johnson <johnson.kat.g@gmail.com>
2022-02-09 23:48:14 -0500

    prompts work and objects created

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const questions = require('./lib/questions')
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
@@ -9,100 +10,95 @@ function init(employeeData) {
     if (!employeeData) {
         employeeData = [];
     }
-    inquirer.prompt ([
-        {
-            type: 'checkbox',
-            name: 'role',
-            message: 'Please select a role for the new employee',
-            choices: ['Manager', 'Engineer', 'Intern']
-        },
-    ])
-    .then(({role}) => {
-        if ({ role: [ 'Engineer' ] }) {
-            {   this.type = new Engineer()
-                this.engineer.getName();
-                this.engineer.getId();
-                this.engineer.getEmail();
-                this.engineer.getOffice();
-                this.engineer.getRole()
-                this.engineer.getGithub();
-                employeeData.push(this);
-            }
-            inquirer.prompt([
-                {
-                    type: 'confirm',
-                    name: 'confirmAdd',
-                    message: 'Wold you like to enter another employee',
-                    default: false
-                }
-            ])
-            .then(({confirmAdd}) => {
-                if (confirmAdd) {
-                    newEmployee.init(employeeData);
-                } 
-                else {
-                    return (employeeData);
-                }
+    inquirer.prompt(questions.startQuestions)
+    .then((answers) => {
+        if (answers.role === 'Engineer') {
+            let engineer = new Engineer (
+                answers.name,
+                answers.id,
+                answers.email,
+            )
+            inquirer.prompt(questions.engineerQuestions)
+            .then((response) => {
+                engineer = { ...engineer, ...response };
+                console.log(engineer)
+                employeeData.push(engineer)
+                inquirer.prompt([
+                    {
+                        type: 'confirm',
+                        name: 'confirmAdd',
+                        message: 'Wold you like to enter another employee',
+                        default: false
+                    }
+                ])
+                .then(({confirmAdd}) => {
+                    if (confirmAdd) {
+                        init(employeeData);
+                    } 
+                    else {
+                        return (employeeData);
+                    }
             })
-        }
-        if (role === 'Intern') {
-            this.type = new Intern
-            this.name = this.intern.getName();
-            this.id = this.intern.getId();
-            this.email = this.intern.getEmail();
-            this.office = this.intern.getOffice();
-            this.role = this.intern.getRole()
-            this.github = this.intern.getGithub();
-            this.role = this.intern.getRole();
-            this.school = this.intern.getSchool();
-            employeeData.push(this);
-            inquirer.prompt([
-                {
-                    type: 'confirm',
-                    name: 'confirmAdd',
-                    message: 'Wold you like to enter another employee',
-                    default: false
-                }
-            ])
-            .then(({confirmAdd}) => {
-                if (confirmAdd) {
-                    newEmployee.init(employeeData);
-                } 
-                else {
-                    return (employeeData);
-                }
+            }) 
+        } else if (answers.role === 'Intern') {
+            let intern = new Intern (
+                answers.name,
+                answers.id,
+                answers.email,
+            )
+            inquirer.prompt(questions.internQuestions)
+            .then((response) => {
+                intern = { ...intern, ...response };
+                console.log(intern)
+                employeeData.push(intern)
+                inquirer.prompt([
+                    {
+                        type: 'confirm',
+                        name: 'confirmAdd',
+                        message: 'Wold you like to enter another employee',
+                        default: false
+                    }
+                ])
+                .then(({confirmAdd}) => {
+                    if (confirmAdd) {
+                        init(employeeData);
+                    } 
+                    else {
+                        return (employeeData);
+                    }
             })
-        }
-        if (role === 'Manager') {
-            this.type = new Manager
-            this.name = this.manager.getName();
-            this.id = this.manager.getId();
-            this.email = this.manager.getEmail();
-            this.office = this.manager.getOffice();
-            this.role = this.manager.getRole()
-            this.github = this.manager.getGithub();
-            this.role = this.manager.getRole();
-            employeeData.push(this);
-            inquirer.prompt([
-                {
-                    type: 'confirm',
-                    name: 'confirmAdd',
-                    message: 'Wold you like to enter another employee',
-                    default: false
-                }
-            ])
-            .then(({confirmAdd}) => {
-                if (confirmAdd) {
-                    newEmployee.init(employeeData);
-                } 
-                else {
-                    return (employeeData);
-                }
+            })
+        } else if (answers.role === 'Manager') {
+            let manager = new Manager (
+                answers.name,
+                answers.id,
+                answers.email,
+            )
+            inquirer.prompt(questions.managerQuestions)
+            .then((response) => {
+                manager = { ...manager, ...response };
+                console.log(manager)
+                employeeData.push(manager)
+                inquirer.prompt([
+                    {
+                        type: 'confirm',
+                        name: 'confirmAdd',
+                        message: 'Wold you like to enter another employee',
+                        default: false
+                    }
+                ])
+                .then(({confirmAdd}) => {
+                    if (confirmAdd) {
+                        init(employeeData);
+                    } 
+                    else {
+                        return (employeeData);
+                    }
+            })
             })
         }
     })
 }
-
 init()
 // .then (portfolioData => {
 //     return generatePage(portfolioData)
